59958884889486338dc4af719729393e7e0a024d
kwanele <kwaneleshelembe841@gmail.com>
2022-09-05 17:14:05 +0200

    added navItems logic and styled cover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-const rellax=new Rellax(".rellax");
+const navItems=document.querySelectorAll(".nav-item");
+const navToggler=document.querySelector("#navbar-toggler-btn");
+
+let activeItem=navItems[0];
+
+navItems.forEach(n=>{
+		n.addEventListener("click",(event)=>{
+			activeItem.classList.remove("active");
+			n.classList.add("active");
+			activeItem=n;
+			navToggler.click();
+		})
+});
